5bb4074da593699c09b04621bb03d26c642b2578
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-22 00:03:36 +0530

    created helpers utils and updated chats component

--- components/Chats.jsx
+++ components/Chats.jsx
@@ -1,13 +1,43 @@
-import React, { useEffect,useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useChatContext } from "@/context/chatContext";
-import { collection, onSnapshot } from "firebase/firestore";
+import {
+    Timestamp,
+    collection,
+    doc,
+    getDoc,
+    onSnapshot,
+    query,
+    updateDoc,
+    where,
+} from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 import { RiSearch2Line } from "react-icons/ri";
 import Avatar from "./Avatar";
+import { useAuth } from "@/context/authContext";
+import { formateDate } from "@/utils/helpers";
 
 const Chats = () => {
-    const { users, setUsers } = useChatContext();
+    const {
+        users,
+        setUsers,
+        selectedChat,
+        setSelectedChat,
+        chats,
+        setChats,
+        dispatch,
+        data,
+        resetFooterStates,
+    } = useChatContext();
     const [search, setSearch] = useState("");
+    const [unreadMsgs, setUnreadMsgs] = useState({});
+    const { currentUser } = useAuth();
+
+    const isBlockExecutedRef = useRef(false);
+    const isUsersFetchedRef = useRef(false);
+
+    useEffect(() => {
+        resetFooterStates();
+    }, [data?.chatId]);
 
     useEffect(() => {
         onSnapshot(collection(db, "users"), (snapshot) => {
@@ -17,8 +47,124 @@ const Chats = () => {
                 console.log(doc.data());
             });
             setUsers(updatedUsers);
+            if (!isBlockExecutedRef.current) {
+                isUsersFetchedRef.current = true;
+            }
         });
     }, []);
+
+    useEffect(() => {
+        const documentIds = Object.keys(chats);
+        if (documentIds.length === 0) return;
+        const q = query(
+            collection(db, "chats"),
+            where("__name__", "in", documentIds)
+        );
+
+        const unsub = onSnapshot(q, (snapshot) => {
+            let msgs = {};
+            snapshot.forEach((doc) => {
+                if (doc.id !== data.chatId) {
+                    msgs[doc.id] = doc
+                        .data()
+                        .messages.filter(
+                            (m) =>
+                                m?.read === false &&
+                                m?.sender !== currentUser.uid
+                        );
+                }
+                Object.keys(msgs || {})?.map((c) => {
+                    if (msgs[c]?.length < 1) {
+                        delete msgs[c];
+                    }
+                });
+            });
+            setUnreadMsgs(msgs);
+        });
+        return () => unsub();
+    }, [chats, selectedChat]);
+
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(
+                doc(db, "userChats", currentUser.uid),
+                (doc) => {
+                    if (doc.exists()) {
+                        const data = doc.data();
+                        setChats(data);
+
+                        if (
+                            !isBlockExecutedRef.current &&
+                            isUsersFetchedRef.current &&
+                            users
+                        ) {
+                            const firstChat = Object.values(data)
+                                .filter(
+                                    (chat) =>
+                                        !chat.hasOwnProperty("chatDeleted")
+                                )
+                                .sort((a, b) => b.date - a.date)[0];
+
+                            if (firstChat) {
+                                const user = users[firstChat?.userInfo?.uid];
+
+                                handleSelect(user);
+
+                                const chatId =
+                                    currentUser.uid > user.uid
+                                        ? currentUser.uid + user.uid
+                                        : user.uid + currentUser.uid;
+
+                                readChat(chatId);
+                            }
+
+                            isBlockExecutedRef.current = true;
+                        }
+                    }
+                }
+            );
+        };
+        currentUser.uid && getChats();
+    }, [isBlockExecutedRef.current, users]);
+
+    const filteredChats = Object.entries(chats || {})
+        .filter(([, chat]) => !chat.hasOwnProperty("chatDeleted"))
+        .filter(
+            ([, chat]) =>
+                chat?.userInfo?.displayName
+                    .toLowerCase()
+                    .includes(search.toLocaleLowerCase()) ||
+                chat?.lastMessage?.text
+                    .toLowerCase()
+                    .includes(search.toLocaleLowerCase())
+        )
+        .sort((a, b) => b[1].date - a[1].date);
+
+    console.log(filteredChats);
+
+    const readChat = async (chatId) => {
+        const chatRef = doc(db, "chats", chatId);
+        const chatDoc = await getDoc(chatRef);
+
+        let updatedMessages = chatDoc.data().messages.map((m) => {
+            if (m?.read === false) {
+                m.read = true;
+            }
+            return m;
+        });
+        await updateDoc(chatRef, {
+            messages: updatedMessages,
+        });
+    };
+
+    const handleSelect = (user, selectedChatId) => {
+        setSelectedChat(user);
+        dispatch({ type: "CHANGE_USER", payload: user });
+
+        if (unreadMsgs?.[selectedChatId]?.length > 0) {
+            readChat(selectedChatId);
+        }
+    };
 
     return(
         <div className="flex flex-col h-full">
