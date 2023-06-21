eb41ab893b690fafc0445e2641f503d205b0e8e1
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-22 00:10:45 +0530

    created new chat component

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { useAuth } from '@/context/authContext';
-import { useRouter } from 'next/router';
-import LeftNav from '@/components/LeftNav';
-import Loader from '@/components/Loader';
-import Chats from '@/components/Chats';
-
-
+import React, { useEffect } from "react";
+import { useAuth } from "@/context/authContext";
+import { useRouter } from "next/router";
+import Loader from "@/components/Loader";
+import LeftNav from "@/components/LeftNav";
+import Chats from "@/components/Chats";
+import Chat from "@/components/Chat";
+import { useChatContext } from "@/context/chatContext";
 const Home = () => {
-  const { signOut, currentUser, isLoading } = useAuth();
-  const router = useRouter();
+    const router = useRouter();
+    const { signOut, currentUser, isLoading } = useAuth();
+    const { data } = useChatContext();
 
+    useEffect(() => {
+        if (!isLoading && !currentUser) {
+            router.push("/login");
+        }
+    }, [currentUser, isLoading]);
 
+    return !currentUser ? (
+        <Loader />
+    ) : (
+        <div className="bg-c1 flex h-[100vh]">
+            <div className="flex w-full shrink-0">
+                <LeftNav />
 
-  useEffect(() => {
-    if (!isLoading && !currentUser) {
-        router.push("/login");
-    }
-}, [currentUser, isLoading]);
-
-  return !currentUser ? (
-    <Loader />
-) :  (
-    // <div className='flex w-full bg-black text-white'>
-    //   <button onClick={signOut}>Sign Out</button>
-    // </div>
-    <div className="bg-c1 flex h-[100vh]">
-    <div className="flex w-full shrink-0">
-        <LeftNav />
-        <div className="flex bg-c2 grow">
-            <div className="w-[400px] p-5 overflow-auto scrollbar shrink-0 border-r border-white/[0.05]">
-                <div className="flex flex-col h-full">
-                    <Chats />      
+                <div className="flex bg-c2 grow">
+                    <div className="w-[400px] p-5 overflow-auto scrollbar shrink-0 border-r border-white/[0.05]">
+                        <div className="flex flex-col h-full">
+                            <Chats />
+                        </div>
+                    </div>
+                    {data.user && <Chat />}
                 </div>
             </div>
-            <div>Chat</div>
         </div>
-    </div>
-</div>
-  );
-}
+    );
+};
 
 export default Home;
