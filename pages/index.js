d34c34c4ae04c0b798ce28cb45f2df15ccc4162c
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-21 14:31:05 +0530

    creating the homepage layouts and functionality

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '@/context/authContext';
 import { useRouter } from 'next/router';
+import LeftNav from '@/components/LeftNav';
 
 
 const Home = () => {
@@ -15,12 +16,24 @@ const Home = () => {
     }
 }, [currentUser, isLoading]);
 
-
-
   return (
-    <div className='flex w-full bg-black text-white'>
-      <button onClick={signOut}>Sign Out</button>
+    // <div className='flex w-full bg-black text-white'>
+    //   <button onClick={signOut}>Sign Out</button>
+    // </div>
+    <div className="bg-c1 flex h-[100vh]">
+    <div className="flex w-full shrink-0">
+        <LeftNav />
+        <div className="flex bg-c2 grow">
+            <div className="w-[400px] p-5 overflow-auto scrollbar shrink-0 border-r border-white/[0.05]">
+                <div className="flex flex-col h-full">
+                    {/* <Chats /> */}
+                    side
+                </div>
+            </div>
+            <div>Chat</div>
+        </div>
     </div>
+</div>
   );
 }
 
