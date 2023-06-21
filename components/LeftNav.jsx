936d44491433796f82069760384b4f7702a730f0
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-21 14:50:39 +0530

    created the icon compoentn for the sidenav

--- components/LeftNav.jsx
+++ components/LeftNav.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { BiEdit, BiCheck } from "react-icons/bi";
-// import Avatar from "./Avatar";
+import Avatar from "./Avatar";
 import { useAuth } from "@/context/authContext";
-// import Icon from "./Icon";
+import Icon from "./Icon";
 
 import { FiPlus } from "react-icons/fi";
 import { IoLogOutOutline, IoClose } from "react-icons/io5";
@@ -141,14 +141,16 @@ const LeftNav = () => {
         return (
             <div className="relative flex flex-col items-center">
                 <ToastMessage />
-                {/* <Icon
+                <Icon
                     size="small"
                     className="absolute top-0 right-5 hover:bg-c2"
                     icon={<IoClose size={20} />}
                     onClick={() => setEditProfile(false)}
-                /> */}
+                />
                 <div className="relative group cursor-pointer">
-                    {/* <Avatar size="xx-large" user={currentUser} /> */}
+                    <Avatar size="xx-large"
+                     user={currentUser}
+                      />
                     <div className="w-full h-full rounded-full bg-black/[0.5] absolute top-0 left-0 justify-center items-center hidden group-hover:flex">
                         <label htmlFor="fileUpload">
                             {currentUser.photoURL ? (
@@ -239,7 +241,7 @@ const LeftNav = () => {
                     className="relative group cursor-pointer"
                     onClick={() => setEditProfile(true)}
                 >
-                    {/* <Avatar size="large" user={currentUser} /> */}
+                    <Avatar size="large" user={currentUser} />
                     <div className="w-full h-full rounded-full bg-black/[0.5] absolute top-0 left-0 justify-center items-center hidden group-hover:flex">
                         <BiEdit size={14} />
                     </div>
@@ -251,7 +253,7 @@ const LeftNav = () => {
                     editProfile ? "ml-5" : "flex-col items-center"
                 }`}
             >
-                {/* <Icon
+                <Icon
                     size="x-large"
                     className="bg-green-500 hover:bg-gray-600"
                     icon={<FiPlus size={24} />}
@@ -262,7 +264,7 @@ const LeftNav = () => {
                     className="hover:bg-c2"
                     icon={<IoLogOutOutline size={24} />}
                     onClick={signOut}
-                /> */}
+                />
             </div>
             {/* {usersPopup && (
                 <UsersPopup
