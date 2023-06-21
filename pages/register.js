36a33a127ac99392a8b08ae39b26da9da778a47c
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-21 11:23:37 +0530

    register page and reset password functionality fully completed

--- pages/register.js
+++ pages/register.js
@@ -1,47 +1,64 @@
 import { useEffect, useState } from "react";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
-// import { useAuth } from "@/context/authContext";
-// import Loader from "@/components/Loader";
+import { useAuth } from "@/context/authContext";
+import Loader from "@/components/Loader";
 
-// import {
-//     signInWithEmailAndPassword,
-//     GoogleAuthProvider,
-//     FacebookAuthProvider,
-//     signInWithPopup,
-//     sendPasswordResetEmail,
-// } from "firebase/auth";
-// import { auth } from "@/firebase/firebase";
-// const gProvider = new GoogleAuthProvider();
-// const fProvider = new FacebookAuthProvider();
+import {
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    signInWithPopup,
+    createUserWithEmailAndPassword,
+    sendPasswordResetEmail,
+} from "firebase/auth";
+import { auth } from "@/firebase/firebase";
+const gProvider = new GoogleAuthProvider();
+const fProvider = new FacebookAuthProvider();
 
 import { IoLogoGoogle, IoLogoFacebook } from "react-icons/io";
-// import ToastMessage from "@/components/ToastMessage";
-// import { toast } from "react-toastify";
+import ToastMessage from "@/components/ToastMessage";
+import { toast } from "react-toastify";
 import Link from "next/link";
+import {profileColors} from "@/utils/constants";
 
 const Register = () => {
-    // const router = useRouter();
-    // const { currentUser, isLoading } = useAuth();
-    // const [email, setEmail] = useState("");
+    const router = useRouter();
+    const { currentUser, isLoading } = useAuth();
 
-    // useEffect(() => {
-    //     if (!isLoading && currentUser) {
-    //         router.push("/");
-    //     }
-    // }, [currentUser, isLoading]);
+    useEffect(() => {
+        if (!isLoading && currentUser) {
+            router.push("/");
+        }
+    }, [currentUser, isLoading]);
 
-    // const handleSubmit = async (e) => {
-    //     e.preventDefault();
-    //     const email = e.target[0].value;
-    //     const password = e.target[1].value;
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const colorIndex = Math.floor(Math.random() * profileColors.length);
 
-    //     try {
-    //         await signInWithEmailAndPassword(auth, email, password);
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
+        try {
+         const {user} = await createUserWithEmailAndPassword(auth, email, password);
+
+        await setDoc(doc(db, "users", user.uid),{
+            uid: user.uid,
+            displayName,
+            email,
+            color: profileColors[colorIndex]
+        })
+
+        await setDoc(doc(db, "userChats", user.uid))
+
+         await updateProfile(user , {
+            displayName,
+         });
+
+         router.push("/");
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     // const resetPassword = async () => {
     //     try {
@@ -64,28 +81,31 @@ const Register = () => {
     //     }
     // };
 
-    // const signInWithGoogle = async () => {
-    //     try {
-    //         await signInWithPopup(auth, gProvider);
-    //     } catch (error) {
-    //         console.error("An error occured", error);
-    //     }
-    // };
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, gProvider);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
 
-    // const signInWithFacebook = async () => {
-    //     try {
-    //         await signInWithPopup(auth, fProvider);
-    //     } catch (error) {
-    //         console.error("An error occured", error);
-    //     }
-    // };
+    const signInWithFacebook = async () => {
+        try {
+            await signInWithPopup(auth, fProvider);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
 
     // return isLoading || (!isLoading && !!currentUser) ? (
     //     <Loader />
     // ) : 
-    return (
+    return isLoading || (!isLoading && 
+        currentUser) ? (
+        <Loader />
+    ) : (
         <div className="h-[100vh] flex justify-center items-center bg-c1">
-            {/* <ToastMessage /> */}
+            <ToastMessage />
             <div className="flex items-center flex-col">
                 <div className="text-center">
                     <div className="text-4xl font-bold">
@@ -98,20 +118,20 @@ const Register = () => {
                 <div className="flex items-center gap-2 w-full mt-10 mb-5">
                     <div
                         className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]"
-                        // onClick={signInWithGoogle}
+                        onClick={signInWithGoogle}
                     >
                         <div className="flex items-center justify-center gap-3 text-white font-semibold bg-c1 w-full h-full rounded-md">
                             <IoLogoGoogle size={24} />
-                            <span>Login with Google</span>
+                            <span>Sign Up with Google</span>
                         </div>
                     </div>
                     <div
                         className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-1/2 h-14 rounded-md cursor-pointer p-[1px]"
-                        // onClick={signInWithFacebook}
+                        onClick={signInWithFacebook}
                     >
                         <div className="flex items-center justify-center gap-3 text-white font-semibold bg-c1 w-full h-full rounded-md">
                             <IoLogoFacebook size={24} />
-                            <span>Login with Facebook</span>
+                            <span>Sign Up with Facebook</span>
                         </div>
                     </div>
                 </div>
@@ -122,21 +142,19 @@ const Register = () => {
                 </div>
                 <form
                     className="flex flex-col items-center gap-3 w-[500px] mt-5"
-                    // onSubmit={handleSubmit}
+                    onSubmit={handleSubmit}
                 >
                   <input
                         type="text"
                         placeholder="Display Name"
                         className="w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3"
                         autoComplete="off"
-                        // onChange={(e) => setEmail(e.target.value)}
                     />
                     <input
                         type="email"
                         placeholder="Email"
                         className="w-full h-14 bg-c5 rounded-xl outline-none border-none px-5 text-c3"
-                        autoComplete="off"
-                        // onChange={(e) => setEmail(e.target.value)}
+                        autoComplete="off"       
                     />
                     <input
                         type="password"
